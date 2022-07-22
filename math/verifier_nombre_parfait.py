b18c1478328e1674c7e902a383d7e246c21faa57
Nguewo Fossong Christian <96875506+ngdream@users.noreply.github.com>
2022-07-22 11:29:56 +0100

    Delete math directory

--- math/verifier_nombre_parfait.py
+++
@@ -1,9 +0,0 @@
-n=int(input("entrez un nombre"))
-s=o
-for k in range(1,n):
-    if n % k==0:
-        s+=k
-if s==n:
-    print("ce nest pas un nombre parfait")
-else:
-    print("ce n'est pas un  nombre parfait")
